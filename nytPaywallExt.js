3f4ad75af9a7a6c99742a7092e7a7d09a4284a70
ronnienelson <87160120+ronnienelson@users.noreply.github.com>
2022-10-09 21:11:33 -0500

    Add files via upload

---
+++ nytPaywallExt.js
@@ -0,0 +1,101 @@
+//new york times specific
+let div=document.getElementsByTagName('div')
+
+//NYT scripts take anywhere between 0-15 seconds to run so this script fires at 5 second intervals
+setTimeout(()=>{
+
+    for(let i=0;i<div.length;i++){
+    
+        if(div[i].classList.contains('css-gx5sib')){
+            div[i].classList.remove('css-gx5sib')
+        }
+        if(div[i].classList.contains('css-5x99bu')){
+            div[i].style.display='none'
+        }
+       if(div[i].classList.contains('css-mcm29f')){
+            div[i].style.overflow='scroll'
+        }
+    
+    
+    }
+    },5000)
+
+    setTimeout(()=>{
+
+        for(let i=0;i<div.length;i++){
+        
+            if(div[i].classList.contains('css-gx5sib')){
+                div[i].classList.remove('css-gx5sib')
+            }
+            if(div[i].classList.contains('css-5x99bu')){
+                div[i].style.display='none'
+            }
+           if(div[i].classList.contains('css-mcm29f')){
+                div[i].style.overflow='scroll'
+            }
+        
+        
+        }
+        },6000)
+
+        setTimeout(()=>{
+
+            for(let i=0;i<div.length;i++){
+            
+                if(div[i].classList.contains('css-gx5sib')){
+                    div[i].classList.remove('css-gx5sib')
+                }
+                if(div[i].classList.contains('css-5x99bu')){
+                    div[i].style.display='none'
+                }
+               if(div[i].classList.contains('css-mcm29f')){
+                    div[i].style.overflow='scroll'
+                }
+            
+            
+            }
+            },7000)
+            
+
+
+    setTimeout(()=>{
+
+        for(let i=0;i<div.length;i++){
+        
+            if(div[i].classList.contains('css-gx5sib')){
+                div[i].classList.remove('css-gx5sib')
+            }
+            if(div[i].classList.contains('css-5x99bu')){
+                div[i].style.display='none'
+            }
+           if(div[i].classList.contains('css-mcm29f')){
+                div[i].style.overflow='scroll'
+            }
+        
+        
+        }
+        },10000)
+
+
+setTimeout(()=>{
+
+for(let i=0;i<div.length;i++){
+
+    if(div[i].classList.contains('css-gx5sib')){
+        div[i].classList.remove('css-gx5sib')
+    }
+    if(div[i].classList.contains('css-5x99bu')){
+        div[i].style.display='none'
+    }
+   if(div[i].classList.contains('css-mcm29f')){
+        div[i].style.overflow='scroll'
+    }
+
+
+}
+},15000)
+
+
+////////////////////////////////////////////////
+
+
